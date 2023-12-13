1b0f9185603964c0628e07a08c9eca8407c34766
progchris00 <prog.christian07@gmail.com>
2023-12-13 22:29:23 +0800

    Display message for days without class

--- Classes/1A_Schedule.cs
+++ Classes/1A_Schedule.cs
@@ -44,20 +44,21 @@ public class Schedule
 
     public static void Today(string DayToday, string DateToday)
     {
-        Console.SetCursorPosition(82, 7); 
+        Console.SetCursorPosition(80, 7); 
         Console.WriteLine($"{DayToday}, {DateToday}");
+        bool Wednesday_B = DayToday == "Wednesday" && Session.GetCurrentUserSection(Session.GetCurrentUser()) == "B";
 
-        if (DayToday == "Sunday")
+        if (DayToday == "Sunday" || Wednesday_B)
         {
-            Console.SetCursorPosition(54, 14); 
+            Console.SetCursorPosition(54, 13); 
             Console.WriteLine("There are no classes scheduled for today.");
         }
         else
         {
             string[] csvFilePath = {};
             csvFilePath = File.ReadAllLines("data/Schedule.csv");
-            
             int top = 13;
+
             foreach(var SubjectCode in csvFilePath)
             {
                 string[] fields = SubjectCode.Split(',');
@@ -65,13 +66,16 @@ public class Schedule
                 string SubjectToday = fields[0].Trim();
                 string UserSection = fields[4].Trim();
                 
-                Console.SetCursorPosition(58, 10); 
-                Console.WriteLine("SUBJECT\t\tTIME");
                 if (SubjectToday == DayToday && UserSection == Session.GetCurrentUserSection(Session.GetCurrentUser())) 
                 {
+
+                    Console.SetCursorPosition(58, 10); 
+                    Console.WriteLine("SUBJECT\t\tTIME");
+
                     string storedSubjectCode = fields[1].Trim();
                     string storedSubjectTitle = fields[2].Trim();
                     string storedTimeDate = fields[3].Trim();
+
                     Console.SetCursorPosition(58, top); 
                     Console.WriteLine($"{storedSubjectCode,-15}{storedTimeDate}");
                     top += 2;
