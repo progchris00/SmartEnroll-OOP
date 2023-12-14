384074f93236df16030d8fa7c07a52710fb8339f
progchris00 <prog.christian07@gmail.com>
2023-12-14 18:09:12 +0800

    Capitalize name and surname when storing on csv

--- Classes/Csv.cs
+++ Classes/Csv.cs
@@ -1,16 +1,20 @@
+using System.Globalization;
+
 namespace Itec102
 {
     public static class Csv
     {
         public static void Register(List<string> information)
         {
+        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
             string filePath = "data/users.csv";
 
             using (StreamWriter sw = File.AppendText(filePath))
             {
                 string HashedPassword = Security.HashPassword(information[3]);
 
-                sw.WriteLine($"{information[0]},{information[1].ToUpper()},{information[2].ToUpper()},{HashedPassword},user,{information[4]},{information[5]},{information[6]}");
+                sw.WriteLine($"{information[0]},{myTI.ToTitleCase(information[1])},{myTI.ToTitleCase(information[2])},{HashedPassword},user,{information[4]},{information[5]},{information[6]}");
             }
         }
     }
