2b06669ca5f8a2a189ffd67c857a0f80fddbdf57
progchris00 <prog.christian07@gmail.com>
2023-12-26 12:31:48 +0800

    Added Speech.Synthesis

--- Classes/Application.cs
+++ Classes/Application.cs
@@ -1,9 +1,13 @@
+using System.Speech.Synthesis;
+
 namespace Itec102.StudentManagementSystem
 {
     public class Application
     {
         public static void Run()
         {
+            Speak("Welcome to SmartEnroll.");
+
             string userState = "Logout";
             string[] choices = { "Login", "Register" };
             string message = "Choose an option: ";
