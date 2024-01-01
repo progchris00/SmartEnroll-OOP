87fc00a5ddcb20804a73a60597efd4f1ec7dc35e
progchris00 <prog.christian07@gmail.com>
2024-01-01 19:18:14 +0800

    Edited Admin.Enrolled

---
+++ Classes/Admin.cs
@@ -0,0 +1,44 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Itec102.StudentManagementSystem
+{
+    public class Admin
+    {
+        public static void Enrolled()
+        {
+            string[] sectionChoices = new string[] { "All", "BSCS 1-A", "BSCS 1-B" };
+
+            string sectionMessage = "View currently enrolled students";
+            string sectionStatus = "Logout";
+
+            string adminState = "Login";
+
+            var Menu = new Menu(sectionChoices, sectionMessage, sectionStatus);
+            int indexChoices = Menu.SectionMenu(adminState);
+
+            Choice.Set(sectionChoices[indexChoices]);
+            Csv.LoadUsers(Choice.Get());
+
+            string[] adminChoices = new string[] { "View again", "Logout"};
+
+            string adminMessage = "Choose an option:";
+            string adminStatus = "Logout";
+
+            var AdminMenu = new Menu(adminChoices, adminMessage, adminStatus);
+
+            int adminChoice = AdminMenu.AdminMenu(adminState);
+            string adminSelectedChoice = adminChoices[adminChoice];
+
+            if (adminSelectedChoice == "View again")
+            {
+                Enrolled();
+            }
+            else
+            {
+                Message.LogoutSuccess();
+                Console.ReadKey();
+                Application.Run();
+            }
+        } 
+    }
+}
