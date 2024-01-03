866793cc5c0113d4034521e56409c7a82365ce7c
progchris00 <prog.christian07@gmail.com>
2024-01-03 21:51:48 +0800

    Created Email.cs

---
+++ Classes/Email.cs
@@ -0,0 +1,16 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace Itec102.StudentManagementSystem
+{
+    public class Email
+    {
+        public static void Send()
+        {
+            string fromMail = "smartenrollmessage@gmail.com";
+            string fromPassword = "atfg ibcs kjfl phrm";
+
+            MailMessage message = new MailMessage();
+        }
+    }
+}
